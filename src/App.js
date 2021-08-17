325f24b7c17fdfb3fe475cb773556cf05187ac03
Azhar Zaman <68331974+TheAzharZaman@users.noreply.github.com>
2021-08-18 00:38:44 +0500

    Project Resetted

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
-import React from "react";
-import HomePage from "./Pages/Homepage";
-import Pricing from "./Pages/Pricing";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import CoachingOverview from "./Pages/Coaching/CoachingOverview";
-import EnrollNow from "./Pages/Coaching/EnrollNow";
-import EnrolledStudents from "./Pages/Coaching/EnrolledStudents";
-import EnrollementSuccess from "./Pages/Coaching/EnrollementSuccess";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div className="App__main__container">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route path="/pricing">
-            <Pricing />
-          </Route>
-          <Route path="/coaching/front-end-development/batch1/overview">
-            <CoachingOverview />
-          </Route>
-          <Route path="/coaching/front-end-development/batch1/enrollment">
-            <EnrollNow />
-          </Route>
-          <Route path="/coaching/front-end-development/batch1/enrollement-successful">
-            <EnrollementSuccess />
-          </Route>
-          <Route path="/coaching/front-end-development/batch1/enrolled-students">
-            <EnrolledStudents />
-          </Route>
-        </Switch>
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
