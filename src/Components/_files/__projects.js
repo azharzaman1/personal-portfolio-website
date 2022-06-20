fd83a1942de3e692ed243fd1842c1219a454a628
Azhar Zaman <68331974+azharzaman1@users.noreply.github.com>
2022-06-20 09:02:32 +0500

    Projects updated - Design improves

--- src/Components/_files/__projects.js
+++ src/Components/_files/__projects.js
@@ -1,24 +1,24 @@
 import { assets as Logos } from "../RecentWork/assets/assets";
 import assets from "./assets/assets";
 
-const GraphqlLogo = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="64"
-    width="64"
-    viewBox="0 0 29.999 30"
-    fill="#e10098"
-    {...props}
-  >
-    <path d="M4.08 22.864l-1.1-.636L15.248.98l1.1.636z" />
-    <path d="M2.727 20.53h24.538v1.272H2.727z" />
-    <path d="M15.486 28.332L3.213 21.246l.636-1.1 12.273 7.086zm10.662-18.47L13.874 2.777l.636-1.1 12.273 7.086z" />
-    <path d="M3.852 9.858l-.636-1.1L15.5 1.67l.636 1.1z" />
-    <path d="M25.922 22.864l-12.27-21.25 1.1-.636 12.27 21.25zM3.7 7.914h1.272v14.172H3.7zm21.328 0H26.3v14.172h-1.272z" />
-    <path d="M15.27 27.793l-.555-.962 10.675-6.163.555.962z" />
-    <path d="M27.985 22.5a2.68 2.68 0 0 1-3.654.981 2.68 2.68 0 0 1-.981-3.654 2.68 2.68 0 0 1 3.654-.981c1.287.743 1.724 2.375.98 3.654M6.642 10.174a2.68 2.68 0 0 1-3.654.981A2.68 2.68 0 0 1 2.007 7.5a2.68 2.68 0 0 1 3.654-.981 2.68 2.68 0 0 1 .981 3.654M2.015 22.5a2.68 2.68 0 0 1 .981-3.654 2.68 2.68 0 0 1 3.654.981 2.68 2.68 0 0 1-.981 3.654c-1.287.735-2.92.3-3.654-.98m21.343-12.326a2.68 2.68 0 0 1 .981-3.654 2.68 2.68 0 0 1 3.654.981 2.68 2.68 0 0 1-.981 3.654 2.68 2.68 0 0 1-3.654-.981M15 30a2.674 2.674 0 1 1 2.674-2.673A2.68 2.68 0 0 1 15 30m0-24.652a2.67 2.67 0 0 1-2.674-2.674 2.67 2.67 0 1 1 5.347 0A2.67 2.67 0 0 1 15 5.347" />
-  </svg>
-);
+// const GraphqlLogo = (props) => (
+//   <svg
+//     xmlns="http://www.w3.org/2000/svg"
+//     height="64"
+//     width="64"
+//     viewBox="0 0 29.999 30"
+//     fill="#e10098"
+//     {...props}
+//   >
+//     <path d="M4.08 22.864l-1.1-.636L15.248.98l1.1.636z" />
+//     <path d="M2.727 20.53h24.538v1.272H2.727z" />
+//     <path d="M15.486 28.332L3.213 21.246l.636-1.1 12.273 7.086zm10.662-18.47L13.874 2.777l.636-1.1 12.273 7.086z" />
+//     <path d="M3.852 9.858l-.636-1.1L15.5 1.67l.636 1.1z" />
+//     <path d="M25.922 22.864l-12.27-21.25 1.1-.636 12.27 21.25zM3.7 7.914h1.272v14.172H3.7zm21.328 0H26.3v14.172h-1.272z" />
+//     <path d="M15.27 27.793l-.555-.962 10.675-6.163.555.962z" />
+//     <path d="M27.985 22.5a2.68 2.68 0 0 1-3.654.981 2.68 2.68 0 0 1-.981-3.654 2.68 2.68 0 0 1 3.654-.981c1.287.743 1.724 2.375.98 3.654M6.642 10.174a2.68 2.68 0 0 1-3.654.981A2.68 2.68 0 0 1 2.007 7.5a2.68 2.68 0 0 1 3.654-.981 2.68 2.68 0 0 1 .981 3.654M2.015 22.5a2.68 2.68 0 0 1 .981-3.654 2.68 2.68 0 0 1 3.654.981 2.68 2.68 0 0 1-.981 3.654c-1.287.735-2.92.3-3.654-.98m21.343-12.326a2.68 2.68 0 0 1 .981-3.654 2.68 2.68 0 0 1 3.654.981 2.68 2.68 0 0 1-.981 3.654 2.68 2.68 0 0 1-3.654-.981M15 30a2.674 2.674 0 1 1 2.674-2.673A2.68 2.68 0 0 1 15 30m0-24.652a2.67 2.67 0 0 1-2.674-2.674 2.67 2.67 0 1 1 5.347 0A2.67 2.67 0 0 1 15 5.347" />
+//   </svg>
+// );
 
 export const projects = [
   {
@@ -180,9 +180,29 @@ export const projects = [
     ],
     logoClasses: "w-10 -translate-y-2",
   },
-
   {
     order: 5,
+    title: "Netflix demo",
+    logo: Logos.NetflixLogo,
+    logoAlt: "Netflix Logo",
+    src: "https://netflixdemo.azharzaman.com/",
+    github: "https://github.com/azharzaman1/netflix-portfolio-clone",
+    desc: "Built Netflix listing page, finding movies database from tmdb. Got an idea from yt tutorial(clever programmer), but finished on my own.",
+    techsLogos: [
+      {
+        title: "javascript",
+        Logo: <assets.JavaScriptLogo className="w-5 h-5" />,
+      },
+      { title: "reactjs", Logo: <assets.ReactLogo className="w-5 h-5" /> },
+      {
+        title: "mui(material-ui)",
+        Logo: <assets.MuiLogo className="w-5 h-5" />,
+      },
+    ],
+    logoClasses: "w-24 -translate-y-1",
+  },
+  {
+    order: 6,
     title: "Airbnb demo",
     logo: Logos.AirbnbLogo,
     logoAlt: "Airbnb Logo",
@@ -209,7 +229,7 @@ export const projects = [
     logoClasses: "w-24 -translate-y-1",
   },
   {
-    order: 6,
+    order: 7,
     title: "Personal Portfolio Site",
     logo: Logos.MyLogo,
     logoAlt: "Azhar Logo",
@@ -232,7 +252,7 @@ export const projects = [
     logoClasses: "w-24 -translate-y-2",
   },
   {
-    order: 7,
+    order: 8,
     title: "Single Product eCommerce site (development)",
     logo: Logos.Code,
     logoAlt: "Library application logo",
@@ -266,26 +286,7 @@ export const projects = [
     ],
     logoClasses: "w-10 -translate-y-2",
   },
-  {
-    order: 9,
-    title: "Netflix demo",
-    logo: Logos.NetflixLogo,
-    logoAlt: "Netflix Logo",
-    src: "https://netflixdemo.azharzaman.com/",
-    desc: "Built Netflix listing page, finding movies database from tmdb. Got an idea from yt tutorial(clever programmer), but finished on my own.",
-    techsLogos: [
-      {
-        title: "javascript",
-        Logo: <assets.JavaScriptLogo className="w-5 h-5" />,
-      },
-      { title: "reactjs", Logo: <assets.ReactLogo className="w-5 h-5" /> },
-      {
-        title: "mui(material-ui)",
-        Logo: <assets.MuiLogo className="w-5 h-5" />,
-      },
-    ],
-    logoClasses: "w-24 -translate-y-1",
-  },
+
   // {
   //   order: 8,
   //   title: "Google Demo",
